5f994d70d92fcba38e740c91eac47e1b4cbc5c15
janeskil1525 <janeskil1525@gmail.com>
2025-02-04 17:50:21 +0100

    View added

---
+++ lib/Daje/Workflow/GenerateSQL/Script/View.pm
@@ -0,0 +1,12 @@
+package Daje::Workflow::GenerateSQL::Script::View;
+use Mojo::Base 'Daje::Workflow::GenerateSQL::Base::Common', -base, -signatures;
+
+
+sub generate($self) {
+
+
+    my $test = 1;
+}
+
+
+1;
